c9b3a66beff3b274ff79757c8d631a4b579ec6d0
Tomas Isakowitz <tisakowi@gmail.com>
2013-08-11 14:29:04 +0000

    edited web.js to read index.html

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
+var buf = new Buffer(16);
+buf = fs.readFileSync("index.html");
+mesg = buf.toString();
+
+
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(mesg);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
